68c8c8668cdff172500e2cd91657d4209253e85d
kristian-green-byu <144286975+kristian-green-byu@users.noreply.github.com>
2023-11-11 23:25:04 -0700

    Add files via upload

--- cs260hiked-html/index.js
+++ cs260hiked-html/index.js
@@ -1,43 +1,61 @@
-function login() {
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "miles.html";
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.get('/miles', (_req, res) => {
+  res.send(scores);
+});
+
+let scores = {};
+apiRouter.get('/scores', (_req, res) => {
+  res.send(scores);
+});
+
+
+apiRouter.post('/updateMiles', (req, res) => {
+  const { userName, miles } = req.body;
+  if (!userName || !miles) {
+    return res.status(400).send('Bad Request');
   }
-  function updateLeaderboard() {
-    const leaderboardTable = document.getElementById('leaderboard');
-    leaderboardTable.innerHTML = ''; // Clear existing rows
+  let userMiles = scores[userName] || 0;
+  userMiles += miles;
+  scores[userName] = userMiles;
+  res.send({ [userName]: userMiles });
+  console.log({ [userName]: userMiles });
+});
 
-    // Initialize an array to store user data
-    const userData = [];
+let leaderboardData = {};
 
-    // Iterate through local storage to collect user data
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-
-        // Skip the 'userName' key and entries without valid miles data
-        if (key !== 'userName' && !isNaN(localStorage.getItem(key))) {
-            userData.push({ name: key, miles: parseInt(localStorage.getItem(key)) });
-        }
+apiRouter.post('/updateLeaderboard', (req, res) => {
+    const data = req.body;
+    if (!data) {
+        return res.status(400).send('Bad Request');
     }
-
-    // Sort the user data by miles in descending order
-    userData.sort((a, b) => b.miles - a.miles);
-
-    // Populate the leaderboard table with the sorted user data
-    userData.forEach((entry, index) => {
-        const row = `<tr>
-            <td>${index + 1}</td>
-            <td>${entry.name}</td>
-            <td>${entry.miles}</td>
-        </tr>`;
-        leaderboardTable.innerHTML += row;
-    });
-}
-
-// Call the updateLeaderboard function to populate the leaderboard initially
-updateLeaderboard();
+    leaderboardData = data;
+    res.send({ leaderboardData });
+    console.log({ leaderboardData });
+});
 
 
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {  
+  res.sendFile('index.html', { root: 'public' });
+});
 
 
