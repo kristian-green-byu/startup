1c56b12c03297bd9b33bc07f41864e3c0d8341b7
kristian-green-byu <144286975+kristian-green-byu@users.noreply.github.com>
2023-11-18 19:35:38 -0700

    Update for the DB deliverable

--- cs260hiked-html/index.js
+++ cs260hiked-html/index.js
@@ -1,61 +1,110 @@
-const express = require('express');
-const app = express();
+  const express = require('express');
+  const app = express();
+  const { client, scoreCollection } = require('./database.js');
 
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
-
-
-// JSON body parsing using built-in middleware
-app.use(express.json());
-
-// Serve up the front-end static content hosting
-app.use(express.static('public'));
-
-// Router for service endpoints
-var apiRouter = express.Router();
-app.use(`/api`, apiRouter);
-
-apiRouter.get('/miles', (_req, res) => {
-  res.send(scores);
-});
-
-let scores = {};
-apiRouter.get('/scores', (_req, res) => {
-  res.send(scores);
-});
+  const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 
-apiRouter.post('/updateMiles', (req, res) => {
+  // JSON body parsing using built-in middleware
+  app.use(express.json());
+
+  // Serve up the front-end static content hosting
+  app.use(express.static('public'));
+
+  // Router for service endpoints
+  var apiRouter = express.Router();
+  app.use(`/api`, apiRouter);
+
+  /* 
+  apiRouter.get('/miles', (_req, res) => {
+    res.send(scores);
+  });
+
+  let scores = {};
+  apiRouter.get('/scores', (_req, res) => {
+    res.send(scores);
+  });
+  */
+  try {
+  apiRouter.get('/scores', async (_req, res) => {
+    scores = await scoreCollection.find().toArray();
+    res.send(scores);
+  });
+  
+  apiRouter.get('/miles', async (_req, res) => {
+    const leaderboardDoc = await scoreCollection.findOne({ leaderboard: 'leaderboard' });
+    if (leaderboardDoc) {
+      res.send(leaderboardDoc.data);
+    } else {
+      res.send({});
+    }
+  });
+  
+
+/* 
+  apiRouter.post('/updateMiles', (req, res) => {
+    const { userName, miles } = req.body;
+    if (!userName || !miles) {
+      return res.status(400).send('Bad Request');
+    }
+    let userMiles = scores[userName] || 0;
+    userMiles += miles;
+    scores[userName] = userMiles;
+    res.send({ [userName]: userMiles });
+    console.log({ [userName]: userMiles });
+  });
+*/
+apiRouter.post('/updateMiles', async (req, res) => {
   const { userName, miles } = req.body;
   if (!userName || !miles) {
     return res.status(400).send('Bad Request');
   }
-  let userMiles = scores[userName] || 0;
+
+  // Retrieve the user's current score
+  const userScore = await scoreCollection.findOne({ userName });
+  let userMiles = userScore ? userScore.miles : 0;
   userMiles += miles;
-  scores[userName] = userMiles;
+
+  // Update the database
+  await scoreCollection.updateOne(
+    { userName },
+    { $set: { miles: userMiles } }, // Only update the miles
+    { upsert: true }
+  );
+
   res.send({ [userName]: userMiles });
   console.log({ [userName]: userMiles });
 });
 
-let leaderboardData = {};
-
-apiRouter.post('/updateLeaderboard', (req, res) => {
-    const data = req.body;
-    if (!data) {
-        return res.status(400).send('Bad Request');
-    }
-    leaderboardData = data;
-    res.send({ leaderboardData });
-    console.log({ leaderboardData });
-});
 
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
 
-// Return the application's default page if the path is unknown
-app.use((_req, res) => {  
-  res.sendFile('index.html', { root: 'public' });
-});
+/* 
+  apiRouter.post('/updateLeaderboard', (req, res) => {
+      const data = req.body;
+      if (!data) {
+          return res.status(400).send('Bad Request');
+      }
+      leaderboardData = data;
+      res.send({ leaderboardData });
+      console.log({ leaderboardData });
+  });
+*/
 
 
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+
+  // Return the application's default page if the path is unknown
+  app.use((_req, res) => {  
+    res.sendFile('index.html', { root: 'public' });
+  });
+
+
+
+    // database operation
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
--- cs260hiked-html/public/index.js
+++ cs260hiked-html/public/index.js
@@ -3,7 +3,7 @@ function login() {
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "miles.html";
   }
- 
+ /*
   async function updateLeaderboard() {
     const leaderboardTable = document.getElementById('leaderboard');
     leaderboardTable.innerHTML = ''; // Clear existing rows
@@ -12,17 +12,46 @@ function login() {
         // Fetch user data from the backend service
         const response = await fetch('/api/scores');
         const userData = await response.json();
+        console.log(userData);
 
         // Convert the user data into an array and sort it by miles in descending order
         const sortedUserData = Object.entries(userData)
             .map(([name, miles]) => ({ name, miles }))
             .sort((a, b) => b.miles - a.miles);
+            console.log(sortedUserData);
+
+        // Populate the leaderboard table with the sorted user data
+        sortedUserData.forEach((entry, index) => {
+            console.log(entry);
+            const row = `<tr>
+                <td>${index + 1}</td>
+                <td>${entry.name}</td>
+                <td>${entry.miles}</td>
+            </tr>`;
+            leaderboardTable.innerHTML += row;
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+*/
+async function updateLeaderboard() {
+    const leaderboardTable = document.getElementById('leaderboard');
+    leaderboardTable.innerHTML = ''; // Clear existing rows
+
+    try {
+        // Fetch user data from the backend service
+        const response = await fetch('/api/scores');
+        const userData = await response.json();
+
+        // Sort the user data by miles in descending order
+        const sortedUserData = userData.sort((a, b) => b.miles - a.miles);
 
         // Populate the leaderboard table with the sorted user data
         sortedUserData.forEach((entry, index) => {
             const row = `<tr>
                 <td>${index + 1}</td>
-                <td>${entry.name}</td>
+                <td>${entry.userName}</td>
                 <td>${entry.miles}</td>
             </tr>`;
             leaderboardTable.innerHTML += row;
